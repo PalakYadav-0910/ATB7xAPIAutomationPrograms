2b8ecc121a4b85213283a5752acecb23a81fa104
Palak_Yadav <palak.yadav-EXT@lebara.com>
2024-09-20 21:02:38 +0530

    TestNG

---
+++ src/test/java/org/apiautomation/ex_15092024/CRUD/testng/Test001.java
@@ -0,0 +1,20 @@
+package org.apiautomation.ex_15092024.CRUD.testng;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Test001 {
+
+    @Test
+    public void test_get(){
+
+        RestAssured
+                .given().relaxedHTTPSValidation()
+                .baseUri("https://zippopotam.us")
+                .basePath("/IN/560037")
+                .when()
+                .log().all().get()
+                .then()
+                .log().all().statusCode(200);
+    }
+}
